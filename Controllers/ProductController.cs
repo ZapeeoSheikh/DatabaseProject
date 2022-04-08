74d79fd91084aa65b83ff4f3ac33602261b62ef6
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-08 19:15:04 +0500

    controller update

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWebProject.Models;
+
+namespace MyWebProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        MyWebContext db = new MyWebContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
